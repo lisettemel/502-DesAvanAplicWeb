a666548dad1bfc3a9ad333a5563b56e350a6ae85
Lisette <lismelor@gmail.com>
2025-04-06 18:34:45 -0600

    changes made

--- 502/vite-project/src/App.tsx
+++ 502/vite-project/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Clase2 from './class2/A01028066/components/ejemplo';
+import Clase2 from './class2/A01028066/ejemplo';
 import Dashboard from './pages/Dashboard';
 import LoginPage from './pages/LoginPage';
 import { useState } from 'react';
